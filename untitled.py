5ee4ebabc9e863c4938bea637b326a0a92e33928
audrey <audreymccormick@outlook.com>
2016-02-21 11:15:16 -0700

    changing from static css to scss with pyscss

--- untitled.py
+++ untitled.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+from flask.ext.assets import Environment, Bundle
 
 app = Flask(__name__)
+
+assets = Environment(app)
+assets.url = app.static_url_path
+scss = Bundle('style.scss', filters='pyscss', output='all.css')
+assets.register('scss_all', scss)
 
 
 @app.route('/')
