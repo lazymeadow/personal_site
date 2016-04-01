116d6bae5127112212c28ecfbf2a69c5b87948ad
Audrey McCormick <amccormick@cradlepoint.com>
2016-04-01 12:50:28 -0600

    subdomain route example setup

--- main_site/main_site.py
+++ main_site/main_site.py
@@ -3,6 +3,9 @@ from flask.ext.assets import Environment, Bundle
 
 app = Flask(__name__)
 
+#TODO make application configurations, so this isn't hardcoded for dev
+app.config['SERVER_NAME'] = 'audreymavra.localhost:3579'
+
 assets = Environment(app)
 assets.url = app.static_url_path
 scss = Bundle('style.scss', filters='pyscss', output='all.css')
@@ -12,6 +15,11 @@ assets.register('scss_all', scss)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/', subdomain='blog')
+def blog():
+    return "BLOG"
 
 
 if __name__ == '__main__':
